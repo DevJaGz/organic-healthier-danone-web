3c1b37af93bbad1981d3e9e168353b04eb5b17d8
Julian Andres Gomez Gomez <jgomezpersonal7@gmail.com>
2023-06-19 01:03:41 -0500

    Open and close navigation menu

---
+++ src/app/core/services/nav-menu.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class NavigationMenuService {
+	private readonly _isMenuActive$ = new BehaviorSubject<boolean>(false);
+
+	get isMenuActive(): boolean {
+		return this._isMenuActive$.value;
+	}
+
+	get isMenuActive$(): Observable<boolean> {
+		return this._isMenuActive$.asObservable();
+	}
+
+	openMenu(): void {
+		this.updateMenuState(true);
+	}
+
+	closeMenu(): void {
+		this.updateMenuState(false);
+	}
+
+	private updateMenuState(value: boolean): void {
+		console.log('updated', value);
+
+		this._isMenuActive$.next(value);
+	}
+}
--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostListener, inject } from '@angular/core';
 import { CORE_ROUTE_NAME } from '@core/core-routing.module';
+import { NavigationMenuService } from '@core/services/nav-menu.service';
+import { Observable } from 'rxjs';
 
 @Component({
 	selector: 'app-navbar',
@@ -9,4 +11,24 @@ import { CORE_ROUTE_NAME } from '@core/core-routing.module';
 })
 export class NavbarComponent {
 	readonly route = CORE_ROUTE_NAME;
+	private readonly navMenuService = inject(NavigationMenuService);
+
+	get isMenuActive$(): Observable<boolean> {
+		return this.navMenuService.isMenuActive$;
+	}
+
+	openMenu(): void {
+		this.navMenuService.openMenu();
+	}
+
+	closeMenu(): void {
+		this.navMenuService.closeMenu();
+	}
+
+	@HostListener('window:resize')
+	onResize(): void {
+		console.log('Close by resize');
+
+		this.navMenuService.closeMenu();
+	}
 }
